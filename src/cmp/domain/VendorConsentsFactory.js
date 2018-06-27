901d822358f505162583ba6650a03c76c9e45555
alextremp <alex.castells@schibsted.com>
2018-06-27 13:13:47 +0200

    Missing new factory method in previous commit

--- src/cmp/domain/VendorConsentsFactory.js
+++ src/cmp/domain/VendorConsentsFactory.js
@@ -3,8 +3,10 @@
  */
 export default class VendorConsentsFactory {
   createVendorConsents({consent, globalVendorList, allowedVendorIds}) {
-    throw new Error(
-      'VendorConsentsFactory#createVendorConsents must be implemented'
-    )
+    throw new Error('VendorConsentsFactory#createVendorConsents')
+  }
+
+  createFromEncodedConsent({encodedConsent, allowedVendorIds}) {
+    throw new Error('VendorConsentsFactory#createFromEncodedConsent')
   }
 }
