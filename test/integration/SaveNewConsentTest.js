345dd02c0ba524960704749cd259d3ffae6b2ccb
alextremp <alex.castells@schibsted.com>
2018-07-03 17:13:41 +0200

    Add comments to the tested use case and validate the resulting consent

--- test/integration/SaveNewConsentTest.js
+++ test/integration/SaveNewConsentTest.js
@@ -1,3 +1,4 @@
+import {ConsentString} from 'consent-string'
 import {
   CONSENT_STATUS_ACCEPTED,
   CONSENT_STATUS_NOT_ACCEPTED
@@ -5,19 +6,44 @@ import {
 import initializeTestClientCMP from './initializeTestClientCMP'
 
 describe('Save New Consent', () => {
-  it('Should accept a new Vendor Consents object', done => {
+  it('Should accept a new Vendor Consents object and then modifying it', done => {
     const saveConsent = ({cmpClient}) =>
       Promise.resolve()
+        // client should check the status of the consent
         .then(() => cmpClient.getConsentStatus())
-        .then(consentStatus =>
-          filterConsentStatus(consentStatus, CONSENT_STATUS_NOT_ACCEPTED)
+        // the first time, the consent should return NOT_ACCEPTED
+        .then(consentStatusResult =>
+          filterConsentStatus(
+            consentStatusResult.result,
+            CONSENT_STATUS_NOT_ACCEPTED
+          )
         )
+        // in that case, the client should get the global vendor list to show the UI tool to edit the consent and show it
         .then(() =>
-          cmpClient.setVendorConsents(sampleVendorConsentsEditedInAnUI)
-        )
-        .then(() => cmpClient.getConsentStatus())
-        .then(consentStatus =>
-          filterConsentStatus(consentStatus, CONSENT_STATUS_ACCEPTED)
+          cmpClient.getVendorList().then(globalVendorListResult =>
+            Promise.resolve()
+              // after editing the consents, the client should save the vendor consents
+              .then(() =>
+                cmpClient.setVendorConsents(sampleVendorConsentsEditedInAnUI)
+              )
+              // just for validation, now the consent status should be ACCEPTED
+              .then(() => cmpClient.getConsentStatus())
+              .then(consentStatusResult =>
+                filterConsentStatus(
+                  consentStatusResult.result,
+                  CONSENT_STATUS_ACCEPTED
+                )
+              )
+              // any Advertising SDK that wants to get the consent, now will have it accessible getting the encoded consent data
+              .then(() => cmpClient.getConsentData())
+              .then(consentDataResult =>
+                checkEncodedConsent({
+                  encodedConsent: consentDataResult.result.consentData,
+                  acceptedConsents: sampleVendorConsentsEditedInAnUI,
+                  globalVendorList: globalVendorListResult.result
+                })
+              )
+          )
         )
 
     Promise.resolve()
@@ -29,12 +55,49 @@ describe('Save New Consent', () => {
 })
 
 const filterConsentStatus = (consentStatus, status) => {
-  if (consentStatus.result !== status) {
+  if (consentStatus !== status) {
     throw new Error('Consent Status should be: ' + status)
   }
-  return consentStatus.result
+  return consentStatus
 }
 const sampleVendorConsentsEditedInAnUI = {
   purposeConsents: [1, 2, 3, 4],
   vendorConsents: [1, 2, 3, 6, 7, 8]
+}
+const checkEncodedConsent = ({
+  encodedConsent,
+  acceptedConsents,
+  globalVendorList
+}) => {
+  const consent = new ConsentString(encodedConsent)
+
+  // the allowed purposes into the encoded consent should be the edited purposes in the UI
+  globalVendorList.purposes.map(p => p.id).forEach(id => {
+    if (
+      acceptedConsents.purposeConsents.includes(id) &&
+      !consent.isPurposeAllowed(id)
+    ) {
+      throw new Error(`Purpose ${id} should be allowed`)
+    } else if (
+      !acceptedConsents.purposeConsents.includes(id) &&
+      consent.isPurposeAllowed(id)
+    ) {
+      throw new Error(`Purpose ${id} should not be allowed`)
+    }
+  })
+
+  // the allowed vendors into the encoded consent should be the edited vendors in the UI
+  globalVendorList.vendors.map(v => v.id).forEach(id => {
+    if (
+      acceptedConsents.vendorConsents.includes(id) &&
+      !consent.isVendorAllowed(id)
+    ) {
+      throw new Error(`Vendor ${id} should be allowed`)
+    } else if (
+      !acceptedConsents.vendorConsents.includes(id) &&
+      consent.isVendorAllowed(id)
+    ) {
+      throw new Error(`Vendor ${id} should not be allowed`)
+    }
+  })
 }
