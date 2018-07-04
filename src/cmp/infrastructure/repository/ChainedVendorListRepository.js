36a7aa6798aaa6037b741549db004712d6a389d1
alextremp <alex.castells@schibsted.com>
2018-07-04 16:01:52 +0200

    Refactor the chained repository to a more functional readable way

--- src/cmp/infrastructure/repository/ChainedVendorListRepository.js
+++ src/cmp/infrastructure/repository/ChainedVendorListRepository.js
@@ -4,25 +4,39 @@
  */
 export default class ChainedVendorListRepository {
   constructor({inMemoryVendorListRepository, httpVendorListRepository} = {}) {
-    this._inMemoryVendorListRepository = inMemoryVendorListRepository
-    this._httpVendorListRepository = httpVendorListRepository
+    this._getLocalVendorList = getLocalVendorList({
+      inMemoryVendorListRepository
+    })
+    this._getRemoteVendorList = getRemoteVendorList({httpVendorListRepository})
+    this._saveRemoteVendorListToLocal = saveRemoteVendorListToLocal({
+      inMemoryVendorListRepository
+    })
   }
 
   getGlobalVendorList() {
     return Promise.resolve()
-      .then(() => this._inMemoryVendorListRepository.getGlobalVendorList())
+      .then(this._getLocalVendorList)
       .then(
         globalVendorList =>
           globalVendorList ||
-          this._httpVendorListRepository
-            .getGlobalVendorList()
-            .then(globalVendorList =>
-              this._inMemoryVendorListRepository
-                .setGlobalVendorList({
-                  globalVendorList
-                })
-                .then(() => globalVendorList)
+          this._getRemoteVendorList().then(globalVendorList =>
+            this._saveRemoteVendorListToLocal({globalVendorList}).then(
+              () => globalVendorList
             )
+          )
       )
   }
 }
+
+const getLocalVendorList = ({inMemoryVendorListRepository}) => () =>
+  inMemoryVendorListRepository.getGlobalVendorList()
+
+const getRemoteVendorList = ({httpVendorListRepository}) => () =>
+  httpVendorListRepository.getGlobalVendorList()
+
+const saveRemoteVendorListToLocal = ({inMemoryVendorListRepository}) => ({
+  globalVendorList
+}) =>
+  inMemoryVendorListRepository.setGlobalVendorList({
+    globalVendorList
+  })
