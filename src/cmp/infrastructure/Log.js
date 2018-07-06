f136db312659cdb51e95510ca0aeca5d0ba81432
alextremp <alex.castells@schibsted.com>
2018-07-06 13:47:55 +0200

    Add the console methods to better log tracing with log levels
    * supports debug, info, warn, error, off
    * supports having no console declared

--- src/cmp/infrastructure/Log.js
+++ src/cmp/infrastructure/Log.js
@@ -1,18 +1,53 @@
-export default class Log {
-  constructor({console}) {
-    this._console = console
+class Log {
+  constructor({level = LEVEL.off, console} = {}) {
+    this._level = (this._isValidLevel({level}) && level) || LEVEL.off
+    this._console = console || NO_CONSOLE
   }
-
+  changeLevel({level}) {
+    this._level = (this._isValidLevel({level}) && level) || this._level
+  }
+  get level() {
+    return this._level
+  }
   debug(...args) {
-    this._print({level: 'DEBUG', args})
+    if (this._level <= LEVEL.debug) {
+      this._print({logMethod: this._console.log, level: 'DEBUG', args})
+    }
   }
-
+  info(...args) {
+    if (this._level <= LEVEL.info) {
+      this._print({logMethod: this._console.info, level: 'INFO', args})
+    }
+  }
+  warn(...args) {
+    if (this._level <= LEVEL.warn) {
+      this._print({logMethod: this._console.warn, level: 'WARN', args})
+    }
+  }
   error(...args) {
-    this._print({level: 'ERROR', args})
+    if (this._level <= LEVEL.error) {
+      this._print({logMethod: this._console.error, level: 'ERROR', args})
+    }
   }
-
-  _print({level, args}) {
+  _print({logMethod, level, args}) {
     const [message, ...rest] = [...args]
-    this._console.log(`[${level}] CMP - ${message}`, ...rest)
+    logMethod(`[${level}] CMP - ${message}`, ...rest)
+  }
+  _isValidLevel({level}) {
+    return level >= LEVEL.debug && level <= LEVEL.off
   }
 }
+const LEVEL = {
+  debug: 1,
+  info: 2,
+  warn: 3,
+  error: 4,
+  off: 5
+}
+const NO_CONSOLE = {
+  log: () => null,
+  info: () => null,
+  warn: () => null,
+  error: () => null
+}
+export {Log, LEVEL}
