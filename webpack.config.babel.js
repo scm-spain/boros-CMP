590dace08bd19d47b74c7302e7d16a9d0e0aeb8e
rafael-marquez <rafael.marquez@scmspain.com>
2018-08-01 11:52:02 +0200

    - apply best practice to avoid magic strings, now constants are used.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,6 +2,11 @@ import webpack from 'webpack'
 import path from 'path'
 import CleanWebpackPlugin from 'clean-webpack-plugin'
 
+const WEBPACK_MODE_PRODUCTION = 'production'
+const OUTPUT_FILENAME_DEV = 'cmp.dev.js'
+const OUTPUT_FILENAME_PRO = 'cmp.pro.js'
+const OUTPUT_DIST_FOLDER = 'dist'
+
 const getMajorVersionFromPackageJsonVersion = () => {
   return JSON.stringify(process.env.npm_package_version.split('.')[0])
 }
@@ -9,8 +14,8 @@ const getMajorVersionFromPackageJsonVersion = () => {
 let webpackConfig = {
   entry: './src/index.js',
   output: {
-    path: path.resolve('dist'),
-    filename: 'cmp.dev.js',
+    path: path.resolve(OUTPUT_DIST_FOLDER),
+    filename: OUTPUT_FILENAME_DEV,
     libraryTarget: 'umd'
   },
   module: {
@@ -28,13 +33,13 @@ let webpackConfig = {
     new webpack.DefinePlugin({
       CMP_VERSION: getMajorVersionFromPackageJsonVersion()
     }),
-    new CleanWebpackPlugin(['dist'], {verbose: true})
+    new CleanWebpackPlugin([OUTPUT_DIST_FOLDER], {verbose: true})
   ]
 }
 
 module.exports = (env, argv) => {
-  if (argv.mode === 'production') {
-    webpackConfig.output.filename = 'cmp.pro.js'
+  if (argv.mode === WEBPACK_MODE_PRODUCTION) {
+    webpackConfig.output.filename = OUTPUT_FILENAME_PRO
   }
   return webpackConfig
 }
