0c919e06af6fd814b0b6c5b01615124697f0975b
alextremp <alex.castells@schibsted.com>
2018-08-20 15:11:36 +0200

    Remove the need of calling the polyfill with the window each time

--- src/cmp/infrastructure/createEvent.js
+++ src/cmp/infrastructure/createEvent.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-undef */
-import customEventPolyfill from './event-polyfill'
 
 const createEvent = ({window, name, detail = {}} = {}) => {
-  customEventPolyfill(window)
   window.document.dispatchEvent(new CustomEvent(name, {detail}))
 }
 
--- src/cmp/infrastructure/event-polyfill.js
+++ src/cmp/infrastructure/event-polyfill.js
@@ -1,23 +1,19 @@
-const customEventPolyfill = window => {
-  if (typeof window.CustomEvent !== 'function') {
-    const CustomEvent = (event, params) => {
-      const eventParams = params || {
-        bubbles: false,
-        cancelable: false,
-        detail: undefined
-      }
-      let evt = document.createEvent('CustomEvent')
-      evt.initCustomEvent(
-        event,
-        eventParams.bubbles,
-        eventParams.cancelable,
-        eventParams.detail
-      )
-      return evt
+if (window && typeof window.CustomEvent !== 'function') {
+  const CustomEvent = (event, params) => {
+    const eventParams = params || {
+      bubbles: false,
+      cancelable: false,
+      detail: undefined
     }
-    CustomEvent.prototype = window.Event.prototype
-    window.CustomEvent = CustomEvent
+    let evt = document.createEvent('CustomEvent')
+    evt.initCustomEvent(
+      event,
+      eventParams.bubbles,
+      eventParams.cancelable,
+      eventParams.detail
+    )
+    return evt
   }
+  CustomEvent.prototype = window.Event.prototype
+  window.CustomEvent = CustomEvent
 }
-
-export default customEventPolyfill
