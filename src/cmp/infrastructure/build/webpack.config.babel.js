b1fa0634abd6256caf55bfb62b82972a95a21231
rafamarquezv <rafamarquezv@users.noreply.github.com>
2018-08-21 13:51:23 +0200

    Hotfix/remove duplicated webpack config file (#31)
    * Fix duplicated webpack config file:
    
    * remove the old one from build folder
    * move the new one from root to build folder
    * update package.json build and dev tasks to point to webpack config file located now inside the build folder
    * add CleanWebpackPlugin root as now dist folder is not located outside build folder
    
    * * use constants to define all sensitive data
    * use only one webpack config object with multiple entries
    
    * * Divide configuration for both apps
    * Modify tasks to be more app specific
    * Update readme file
    
    * * rename constant

--- src/cmp/infrastructure/build/webpack.config.babel.js
+++ src/cmp/infrastructure/build/webpack.config.babel.js
@@ -3,16 +3,17 @@ import path from 'path'
 import CleanWebpackPlugin from 'clean-webpack-plugin'
 
 const WEBPACK_MODE_PRODUCTION = 'production'
+const OUTPUT_DIST_FOLDER = 'dist'
+const ENTRY_PATH = './src/index.js'
 const OUTPUT_FILENAME_DEV = 'cmp.dev.js'
 const OUTPUT_FILENAME_PRO = 'cmp.pro.js'
-const OUTPUT_DIST_FOLDER = 'dist'
 
 const getMajorVersionFromPackageJsonVersion = () => {
   return JSON.stringify(process.env.npm_package_version.split('.')[0])
 }
 
 let webpackConfig = {
-  entry: './src/index.js',
+  entry: ENTRY_PATH,
   output: {
     path: path.resolve(OUTPUT_DIST_FOLDER),
     filename: OUTPUT_FILENAME_DEV,
@@ -33,7 +34,10 @@ let webpackConfig = {
     new webpack.DefinePlugin({
       CMP_VERSION: getMajorVersionFromPackageJsonVersion()
     }),
-    new CleanWebpackPlugin([OUTPUT_DIST_FOLDER], {verbose: true})
+    new CleanWebpackPlugin([OUTPUT_DIST_FOLDER], {
+      verbose: true,
+      root: process.cwd()
+    })
   ]
 }
 
