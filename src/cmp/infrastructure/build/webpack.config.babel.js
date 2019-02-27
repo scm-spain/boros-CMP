f5d50aaf183f5744cdc34293343359d2821c1d2d
Miguel Angel Duran Garcia <miduga@gmail.com>
2019-02-27 11:38:39 +0100

    Move to babel@7 and export library

--- src/cmp/infrastructure/build/webpack.config.babel.js
+++ src/cmp/infrastructure/build/webpack.config.babel.js
@@ -1,6 +1,6 @@
-import webpack from 'webpack'
-import path from 'path'
-import CleanWebpackPlugin from 'clean-webpack-plugin'
+const webpack = require('webpack')
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 const WEBPACK_MODE_PRODUCTION = 'production'
 const OUTPUT_DIST_FOLDER = 'dist'
