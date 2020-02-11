48468df1af93bd11feafceb0aaf7b886ea836193
maryam.malek <maryam.malek@adevinta.com>
2020-02-11 10:56:19 +0100

    add contentWindow to have acces to postMessage

--- src/cmp/infrastructure/service/iframe_communication/IframeCommunicationClient.js
+++ src/cmp/infrastructure/service/iframe_communication/IframeCommunicationClient.js
@@ -35,7 +35,7 @@ export default class IframeCommunicationClient {
         }
       })
 
-      this._target.postMessage(
+      this._target.contentWindow.postMessage(
         requestBuilder({
           callId: transactionId,
           command: request.command,
